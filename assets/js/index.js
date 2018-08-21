b34354963948482866629886dc8444e433009901
Adrian <adrian.miranda.r@gmail.com>
2018-08-21 14:33:32 -0300

    rediccionamiento a pag de inicio mediante JS

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	//aca se ingresa todo el codigo
+	alert('hola');
+
+	var url = "inicio.html"; 
+$(location).attr('href',url);
+	setTimeout ("redireccionar()", 4000);
+	
+});
