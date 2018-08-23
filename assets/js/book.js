0d94438ca7734bffbcb4901791b25bd63030e132
Adrian <adrian.miranda.r@gmail.com>
2018-08-23 20:49:25 -0300

    prueba con pluggins de bootstrap

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	alert('pagina book')
 });
+
+
+/*    position: fixed;
+    top: 10px;
+    bottom: 10px;
+    z-index: 999;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 90vh;
+
+    */
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	//aca se ingresa todo el codigo
 	alert('hola');
 
-	var url = "inicio.html"; 
-$(location).attr('href',url);
-	setTimeout ("redireccionar()", 4000);
+	setTimeout(function(){
+		var url = "inicio.html"; 
+		$(location).attr('href',url);
+	}, 3000);
 	
+
 });
